a6d108eada0ff21ba644a6b82c44d67fa5c222ee
adaur <adaur.underground@gmail.com>
2013-08-24 23:06:48 +0200

    Fix if PMs are disabled for an user

--- files/plugins/apms/profile_add4.php
+++ files/plugins/apms/profile_add4.php
@@ -1,4 +1,4 @@
 <?php
-if ($user['use_pm'] == '1')
+if ($user['use_pm'] == '1' && $user['g_pm'] == '1')
 				$email_field .= '<p><a href="pms_send.php?uid='.$id.'">'.$lang_pms['Quick message'].'</a> - <a href="pms_contacts.php?add='.$id.'">'.$lang_pms['Add to contacts'].'</a></p>'."\n";
 ?>
