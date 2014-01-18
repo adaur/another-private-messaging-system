6d009426ca2a6e4d94560237d6010076c5c9715e
BrLi <brli7848@localhost.localdomain>
2014-01-18 08:54:54 +0800

    add translation

---
+++ files/lang/Chinese_Taiwan/pms.php
@@ -0,0 +1,143 @@
+<?php
+
+// Language definitions used by the Private Message System-mod
+$lang_pms = array(
+
+// Common
+'Login required'		=>	'您必須登入才能使用私人訊息系統',
+'Disabled PM'			=>	'您已關閉私人訊息系統',
+'Private Messages'		=>	'私人訊息',
+'PM'				=>	'<acronym title="Private Message">私人訊息</acronym>',
+'Quick message'			=>	'傳送私人訊息',
+'Write message'			=>	'傳送新訊息',
+'Inbox'				=>	'收件匣',
+'Outbox'			=>	'寄件匣',
+'Contacts'			=>	'聯絡人',
+'Delete'			=>	'刪除',
+'Please confirm'		=>	'請確認',
+'New message'			=>	'您有一封未閱讀的私人訊息',
+'New messages'			=>	'您有 %s 封未閱讀的私人訊息',
+'See new'			=>	'閱讀新訊息',
+'See news'			=>	'閱讀新訊息',
+'No new'			=>	'無新訊息',
+'Full boxes'			=>	'您的收件匣已滿',
+'Empty boxes'			=>	'您的收件匣是空的',
+'Full to'			=>	'收件匣已達 %s',
+'Select'			=>	'選取',
+'For select'			=>	'對選取的項目：',
+'Messages'			=>	'訊息',
+'OK'				=>	'OK',
+'PM Menu'			=>	'私人訊息',
+'Sending lists'			=>	'寄件清單',
+
+// List a box
+'Date'				=>	'日期',
+'Subject'			=>	'標題',
+'Sender'			=>	'寄件人',
+'Receiver'			=>	'收件人',
+'Mark as read select'		=>	'標記為已閱讀',
+'Read redirect'			=>	'訊息標記為已閱讀，重新導向中...',
+'Mark as unread select'		=>	'標記為未閱讀',
+'Unread redirect'		=>	'訊息標記為未閱讀，重新導向中...',
+'Mark all'			=>	'標記所有訊息為已閱讀',
+'Read all redirect'		=>	'全部訊息標記為已閱讀，重新導向中...',
+'Must select'			=>	'您必須選取一些訊息',
+'No messages'			=>	'無訊息',
+'Unknown'			=>	'未知',
+
+// View a message
+'View'				=>	'觀看私人討論',
+'Reply'				=>	'回覆',
+'Quote'				=>	'引用',
+'Deleted User'			=>	'刪除使用者',
+'Deleted'			=>	'(已刪除)',
+'With'				=>	'用',
+
+// Send a message
+'Send a message'		=>	'傳送訊息',
+'Send to'			=>	'傳送到',
+'Send multiple'			=>	'你可以將訊息傳送給多個用逗號分個的收件人. 最多：',
+'Save message'			=>	'在已傳送訊息中儲存',
+'Send'				=>	'傳送',
+'Sent redirect'			=>	'訊息已傳送，重新導向中...',
+'No user'			=>	'沒有使用者: " %s"',
+'Dest full'			=>	'%s 收件夾已滿，無法將訊息傳給此使用者',
+'Sender full'			=>	'無法儲存訊息，你的收件夾已滿',
+'Flood'				=>	'訊息傳送間隔最少 %s 秒. 請稍候並重新傳送',
+'Must receiver'			=>	'至少一名收件人',
+'Too many receiver'		=>	'最多同時傳送給 %s 名收件人',
+'User blocked'			=>	'%s 拒絕私人訊息',
+'User disable PM'		=>	'%s 關閉私人訊息',
+'Others'			=>	'其他',
+'RE'				=>	'RE: ',
+'Shared message'		=>	'如果你輸入了多個收件人，請檢查此項目如果想要開啟一個共享討論',
+
+// Multidelete
+'Multidelete'			=>	'刪除多個訊息',
+'Delete messages comply'	=>	'確定要刪除已選擇訊息嗎?',
+'Deleted redirect'		=>	'訊息已刪除. 重新整理中 ...',
+
+// Delete
+'Delete message'		=>	'刪除訊息',
+'Delete message comply'		=>	'確定要刪除此訊息?',
+'Del redirect'			=>	'訊息已刪除. 重新導向中...',
+'Topic warning info'		=>	'此主題將會從你的收信匣中被刪除，但是會被其他收件人所保存',
+'Delete for everybody'		=>	'勾選此選項，你將會從所有收件人中刪除訊息(或主題)',
+
+// Contacts
+'Contacts list'			=>	'聯絡人列表',
+'Rights contact'		=>	'版權聯絡',
+'Authorized messages'		=>	'已授權訊息',
+'Authorize'			=>	'授權',
+'Authorize from'		=>	'授權 %s 來傳送私人訊息',
+'Refused messages'		=>	'拒絕訊息',
+'Refuse'			=>	'拒絕',
+'Refuse from'			=>	'拒絕來自 %s 的私人訊息',
+'Refuse user'			=>	'拒絕來自此聯絡人的私人訊息',
+'Add to contacts'		=>	'加入聯絡人清單',
+'Add contact'			=>	'加入聯絡人',
+'Contact name'			=>	'聯絡人姓名',
+'User already contact'		=>	'此使用者已在您的聯絡人清單中',
+'User cannot use'		=>	'無法使用私人訊息系統',
+'User not exists'		=>	'此使用者似乎不存在',
+'Added contact redirect'	=>	'聯絡人已新增. 重新導向中...',
+'Add'				=>	'新增',
+'Quick message x'		=>	'向 %s 傳送私人訊息',
+'Status redirect'		=>	'聯絡人狀態已編輯. 重新導向中...',
+'Multiples status redirect'	=>	'聯絡人狀態已編輯. 重新導向中...',
+'No contacts'			=>	'沒有聯絡人',
+'Must select contacts'		=>	'必須選擇一些聯絡人',
+
+// Multidelete contacts
+'Multidelete contacts'		=>	'刪除多個聯絡人',
+'Delete contacts comply'	=>	'確定要刪除已選擇聯絡人?',
+'Deleted contacts redirect'	=>	'聯絡人已刪除. 重新導向中...',
+
+// Delete contact
+'Delete x'			=>	'刪除聯絡人 %s',
+'Delete contact confirm'	=>	'確定要刪除此聯絡人?',
+'Deleted contact redirect'	=>	'聯絡人已刪除. 重新導向中...',
+
+// Sending lists
+'List name'			=>	'姓名清單',
+'List usernames'		=>	'使用者名稱清單',
+'No sending lists'		=>	'不送出清單',
+'Add a list'			=>	'新增清單',
+'Multidelete lists'		=>	'刪除多個清單',
+'Delete list confirm'		=>	'確定要刪除這份傳送清單？',
+'Delete lists comply'		=>	'確定要刪除選中的清單？',
+'Deleted list redirect'		=>	'傳送清單已刪除，重新導向中...',
+'Must select lists'		=>	'必須選擇一些清單',
+'Select a list'			=>	'選擇清單',
+'Delete this list'		=>	'刪除此清單',
+'List usernames comma'		=>	'使用者名稱清單 (以逗號分隔)',
+'JS required'			=>	'為了使用這些傳送清單，JavaScript 是必須的',
+'Yourself'			=>	'無法將自己加入傳送清單',
+
+// profile.php
+'use_pm_option'			=>	'啟用私人訊息系統',
+'email_option_infos'		=>	'當開啟這個選項時，有任何新訊息時會寄一封 e-mail',
+'email_option'			=>	'e-mail 私人訊息通知',
+'email_option_full'		=>	'包含私人訊息全文',
+
+);
---
+++ files/lang/Chinese_Taiwan/pms_plugin.php
@@ -0,0 +1,30 @@
+<?php
+
+// Language definitions used by the Private Message System-mod
+$lang_plugin_pms = array(
+
+'Private Messages'			=>		'私人訊息',
+'Description'				=>		'此擴充套件可用來控制關於私人訊息的設定與權限',
+'Settings'				=>		'設定',
+'Options'				=>		'選項',
+'Activation'				=>		'啟動私人訊息',
+'Yes'					=>		'是',
+'No'					=>		'否',
+'Enable or not'				=>		'若選否，則所有私人訊息功能將被關閉',
+'Notification'				=>		'啟動通知',
+'Notify or not'				=>		'允許使用者經由電子郵件接收提醒新私人訊息的通知',
+'Notification pop-up'			=>		'啟動彈出式通知',
+'Pop-up or not'				=>		'允許在論壇版面啟動新私人訊息的彈出式通知',
+'Messages per page'			=>		'每頁顯示訊息數',
+'Number messages per page'		=>		'此為私人訊息列表中每頁將顯示的訊息個數',
+'Number receivers'			=>		'接收者數目',
+'Number max receivers'			=>		'每條私人訊息允許的接收者個數上限',
+'Permissions'				=>		'權限',
+'Permissions information'		=>		'管理員與版主對私人訊息系統並沒有操作限制，相反的，訪客沒有任何權限。最後，您可以調校以下會員群組的權限。',
+'Allow group'				=>		'允許此會員群組使用私人訊息',
+'Information limit 1'			=>		'此群組的最大訊息數限制',
+'Information limit 2'			=>		'群組中每一個訊息匣允許的訊息個數，填入 0 表示無限制。',
+'Redirection'				=>		'選項已更新，重新導向中&#160;…',
+'Save'					=>		'儲存'
+
+);
